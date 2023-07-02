2d850cfad88e55e7e18d2b05e794cba6e727b5c4
alvinary <alvaro.cuestas.ll@gmail.com>
2023-07-02 20:58:36 -0300

    Test Rules.build(), remove print, define TestRules

--- tests/tests.py
+++ tests/tests.py
@@ -4,7 +4,6 @@ from md_rules import Rules
 from examples import *
 
 class Tests:
-
     def test_beamer(self):
         logging.info("Testing toBeamer")
         toBeamer().get_latex()
@@ -13,6 +12,8 @@ class Tests:
         logging.info("Testing toMath")
         toMath().get_latex()
 
+class TestRules:
+
     def test_binarize(self):
         logging.info("Testing Rules.binarize_line()")
         rules = Rules()
@@ -20,14 +21,48 @@ class Tests:
         for line in sample_lines:
             result_lines += rules.binarize_line(line)
         result_lines = set([(n, h, tuple(p), o) for (n, h, p, o, _) in result_lines])
-        print(result_lines)
         assert ('sum', 'number', ('number', 'sum[1]'), 10) in result_lines
         assert ('sum[1]', 'sum[1]', ('plus', 'number'), 10) in result_lines
         assert ('product[1]', 'product[1]', ('times', 'number'), 10) in result_lines
+
+    def test_build(self):
+
+        logging.info("Testing Rules.build()")
+        
+        rules = Rules()
+        result_lines = []
+        for line in sample_lines:
+            result_lines += rules.binarize_line(line)
+        rules.build(result_lines)
+
+        begins_with = rules.begin_with['number']
+        begins_with = set([n for (n, _, _, _, _) in begins_with])
+        assert 'product' in begins_with
+        assert 'substraction' in begins_with
+        assert 'sum' in begins_with
+        assert 'negative' not in begins_with
+        assert 'sum[1]' not in begins_with
+
+        begins_with = rules.begin_with['plus']
+        begins_with = set([n for (n, _, _, _, _) in begins_with])
+        assert 'product' not in begins_with
+        assert 'substraction' not in begins_with
+        assert 'sum[1]' in begins_with
+
+        end_with = rules.end_with['number']
+        end_with = set([n for (n, _, _, _, _) in end_with])
+        assert 'negative' in end_with
+        assert 'product' not in end_with
+        assert 'substraction' not in end_with
+        assert 'product[1]' in end_with
+        assert 'substraction[1]' in end_with
+        assert 'sum' not in end_with
+        assert 'sum[1]' in end_with
 
 
 if __name__ == "__main__":
     # Tests.test_beamer()
     # Tests.test_math()
-    tests = Tests()
+    tests = TestRules()
     tests.test_binarize()
+    tests.test_build()
