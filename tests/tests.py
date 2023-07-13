91f30d5a1a5aece80b219dabd6daa1aaeb43d9b7
alvinary <alvaro.cuestas.ll@gmail.com>
2023-07-13 13:35:21 -0300

    Use new Rules constructor in tests

--- tests/tests.py
+++ tests/tests.py
@@ -27,15 +27,8 @@ class TestRules:
         assert ('product[1]', 'product[1]', ('times', 'number'), 10) in result_lines
 
     def test_build(self):
-
         logging.info("Testing Rules.build()")
-        
-        rules = Rules()
-        result_lines = []
-        for line in test_grammar:
-            result_lines += rules.binarize_line(line)
-        rules.build(result_lines)
-
+        rules = Rules(test_grammar)
         begins_with = rules.begin_with['number']
         begins_with = set([n for (n, _, _, _, _) in begins_with])
         assert 'product' in begins_with
@@ -43,13 +36,11 @@ class TestRules:
         assert 'sum' in begins_with
         assert 'negative' not in begins_with
         assert 'sum[1]' not in begins_with
-
         begins_with = rules.begin_with['plus']
         begins_with = set([n for (n, _, _, _, _) in begins_with])
         assert 'product' not in begins_with
         assert 'substraction' not in begins_with
         assert 'sum[1]' in begins_with
-
         end_with = rules.end_with['number']
         end_with = set([n for (n, _, _, _, _) in end_with])
         assert 'negative' in end_with
@@ -70,14 +61,8 @@ class TestParse:
         return parse
 
     def test_parse(self):
-        
-        parser = Rules()
+        parser = Rules(test_grammar)
         result_lines = []
-        
-        for line in test_grammar:
-            result_lines += parser.binarize_line(line)
-        parser.build(result_lines)
-        
         tokens = "( 3 * 3 ) + ( 2 * ( 3 + 1 ) )".split()
         tags = "lparen digits times digits rparen plus lparen digits times lparen digits plus digits rparen rparen".split()
         parse = self.get_parse(parser, tokens, tags)
@@ -86,7 +71,6 @@ class TestParse:
         value = parse.values[whole_span]
         print(value)
         assert value == 17
-        
         tokens = "- ( - ( 1 + 3 ) * 5 + 17 )".split()
         tags = "minus lparen minus lparen digits plus digits rparen times digits plus digits rparen".split()
         parse = self.get_parse(parser, tokens, tags)
@@ -95,8 +79,6 @@ class TestParse:
         value = parse.values[whole_span]
         print(value)
         assert value == 3
-        
-        
 
 if __name__ == "__main__":
     # Tests.test_beamer()
