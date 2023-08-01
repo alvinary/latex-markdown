140c16aee72cde6b5a20f8208e3ac22f596eb4a3
alvinary <alvaro.cuestas.ll@gmail.com>
2023-08-01 11:53:04 -0300

    Add missing concat operations

--- dsls.py
+++ dsls.py
@@ -133,10 +133,10 @@ math_dsl = [
     ('lvert', 'delim', ('|',), DEFAULT_PRECEDENCE, lambda x : '\\lvert'),
     ('rvert', 'delim', ('|',), DEFAULT_PRECEDENCE, lambda x : '\\rvert'),
     # Subindices, superindices and diacritics
-    ('sub', 'math', ('math', '[_', 'math', '_]'), DEFAULT_PRECEDENCE, lambda x, _, y, __ : x + '_{' y '}'),
-    ('super', 'math', ('math', '[^', 'math', '^]'), DEFAULT_PRECEDENCE, lambda x, _, y, __ : '^{' y '}'),
-    ('short sub', 'math', ('math', 'sub', 'math'), DEFAULT_PRECEDENCE + 4, lambda x, _, y : x + '_{' y '}'),
-    ('short super', 'math', ('math', 'sup', 'math'), DEFAULT_PRECEDENCE + 4, lambda x, _, y : '^{' y '}'),
+    ('sub', 'math', ('math', '[_', 'math', '_]'), DEFAULT_PRECEDENCE, lambda x, _, y, __ : x + '_{' + y + '}'),
+    ('super', 'math', ('math', '[^', 'math', '^]'), DEFAULT_PRECEDENCE, lambda x, _, y, __ : '^{' + y + '}'),
+    ('short sub', 'math', ('math', 'sub', 'math'), DEFAULT_PRECEDENCE + 4, lambda x, _, y : x + '_{' + y + '}'),
+    ('short super', 'math', ('math', 'sup', 'math'), DEFAULT_PRECEDENCE + 4, lambda x, _, y : '^{' + y + '}'),
     # ', ^, bar, hat, tilde
     # Common operations
     ('sum', 'op', ('sum',), DEFAULT_PRECEDENCE, lambda x : '\\sum'),
