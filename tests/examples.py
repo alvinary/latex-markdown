4a8475e6fc15d48db29e0971f3c90df0dd44f13a
alvinary <alvaro.cuestas.ll@gmail.com>
2023-08-04 11:27:15 -0300

    Add missing math delimiters

--- tests/examples.py
+++ tests/examples.py
@@ -83,15 +83,15 @@ math_testcase = '''
 It is easy to prove that eigenspaces are disjoint.
 We said ~v~ is an eigenvector of ~T~ if
 
-	Tx = kx
+	~ Tx = kx ~
 	
-for some k in |K.
+for some ~k~ in ~|K~.
 
-So if Tx = kx and Ty = k'y, if x and y are distinct,
-and y != qx, then k' and k cannot be equal, and the
+So if ~Tx = kx~ and ~Ty = k'y~, if ~x~ and ~y~ are distinct,
+and ~y != qx~, then ~k'~ and ~k~ cannot be equal, and the
 two vectors cannot be elements of the same eigenspace.
 
-Note if y + qx then T(y) = T(qx) = k(qx) = ky, and the
+Note if ~y + qx~ then ~T(y) = T(qx) = k(qx) = ky~, and the
 two vectors do belong to the same eigenspace and have
 the same eigenvalue. So 'scaled versions' of the same
 vector belong to the same eigenspace.
