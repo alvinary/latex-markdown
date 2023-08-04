7c7622a2a6b0bc3aae4e2286a2d75bbc4f6c6da7
alvinary <alvaro.cuestas.ll@gmail.com>
2023-08-04 19:31:01 -0300

    Draft preprocessor for Latex with math

--- latex.py
+++ latex.py
@@ -34,9 +34,10 @@ class LatexMarkdown:
 class Latex(LatexMarkdown):
 
     def __init__(self):
-        self.parser = Rules(latex_dsl)
+        self.parser = Rules(latex_dsl + math_dsl)
         self.special_tokens = latex_tokens
         self.default_token = "text"
+        self.math_parser = Math()
     
     def tag(self, token):
         if token in self.special_tokens:
@@ -53,40 +54,67 @@ class Latex(LatexMarkdown):
     def preprocess(self, text):
         text = f"{BEGIN_DOCUMENT} {text.strip()}\n\n{END_DOCUMENT}"
         text = text.replace(NEWLINE, f" {EXPLICIT_NEWLINE} ")
+        text = text.replace(BEGIN_MATH, f" {BEGIN_MATH} ")
         pretokens = text.split()
         tokens = []
         text_tokens = []
         newline_tokens = []
+        math_tokens = []
+        math_mode = False
+        tagged_tokens = []
         for token in pretokens:
-            is_newline = token == EXPLICIT_NEWLINE
-            is_special = token  in self.special_tokens
-            text_empty = not text_tokens
-            is_break = len(newline_tokens) > 1
-            if not is_newline and is_break:
-                tokens.append(EXPLICIT_BREAK)
-                newline_tokens = []
-            elif not is_newline and newline_tokens:
-                tokens.append(EXPLICIT_NEWLINE)
-                newline_tokens = []
-            if is_special and not is_newline and text_empty:
-                tokens.append(token)
-            elif is_special and not is_newline and text_tokens:
+            if token == BEGIN_MATH and not math_tokens:
+                math_mode = not math_mode
                 tokens.append(" ".join(text_tokens))
-                tokens.append(token)
+                tokens.append(BEGIN_MATH)
                 text_tokens = []
-            elif is_newline and text_tokens:
-                tokens.append(" ".join(text_tokens))
-                newline_tokens.append(token)
-                text_tokens = []
-            elif is_newline and text_empty:
-                newline_tokens.append(token)                
-            else:   
-                text_tokens.append(token)
-        tokens.append(" ".join(text_tokens))
+            elif token == BEGIN_MATH and math_tokens:
+                math_mode = not math_mode
+                math_text = " ".join(math_tokens)
+                tagged_math_tokens = self.math_parser.preprocess(math_text)
+                tokens += tagged_math_tokens
+                tokens.append(BEGIN_MATH)
+                math_tokens = []
+            elif not math_mode:
+                is_newline = token == EXPLICIT_NEWLINE
+                is_special = token  in self.special_tokens
+                text_empty = not text_tokens
+                is_break = len(newline_tokens) > 1
+                if not is_newline and is_break:
+                    tokens.append(EXPLICIT_BREAK)
+                    newline_tokens = []
+                elif not is_newline and newline_tokens:
+                    tokens.append(EXPLICIT_NEWLINE)
+                    newline_tokens = []
+                if is_special and not is_newline and text_empty:
+                    tokens.append(token)
+                elif is_special and not is_newline and text_tokens:
+                    tokens.append(" ".join(text_tokens))
+                    tokens.append(token)
+                    text_tokens = []
+                elif is_newline and text_tokens:
+                    tokens.append(" ".join(text_tokens))
+                    newline_tokens.append(token)
+                    text_tokens = []
+                elif is_newline and text_empty:
+                    newline_tokens.append(token)                
+                else:   
+                    text_tokens.append(token)
+            elif math_mode and token != BEGIN_MATH:
+                math_tokens.append(token)
+        if text_tokens:
+            tokens.append(" ".join(text_tokens))
         if "" in tokens:
             tokens.remove("")
-        tags = [self.tag(t) for t in tokens]
-        tagged_tokens = list(zip(tokens, tags))
+        # Alternate between latex and math tags using the
+        # same delimiter-based criterion
+        tagged_tokens = []
+        for token in tokens:
+            if isinstance(token, tuple):
+                tagged_tokens.append(token)
+            else:
+                tagged_tokens.append((token, self.tag(token)))
+        # print('TAGGED TOKENS:', '\n'.join([str(s) for s in tagged_tokens]))
         return tagged_tokens
 
 class Beamer(LatexMarkdown):
