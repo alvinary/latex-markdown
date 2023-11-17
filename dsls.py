1c239ad0ec7d24970b95927824e7e9dedd02275f
Álvaro <alvaro.cuestas.ll@gmail.com>
2023-11-17 11:07:55 -0300

    Change rule ordering

--- dsls.py
+++ dsls.py
@@ -54,9 +54,10 @@ latex_dsl = [
     ('subsubsection', 'block', ('subsubsection_mark', 'text', 'break'), DEFAULT_PRECEDENCE, lambda _, x, __  : subsubsection(x)),
     # Listings
     ('itemize', 'block', ('items',), DEFAULT_PRECEDENCE, lambda x : itemize(x)),
-    ('items', 'items', ('single_item', 'break'), DEFAULT_PRECEDENCE, lambda x, _ : x),
-    ('items', 'items', ('single_item', 'break', 'items'), DEFAULT_PRECEDENCE, lambda x, y, z : x + y + z),
+    
+    ('items', 'items', ('single_item', 'break', 'items'), DEFAULT_PRECEDENCE + 10, lambda x, y, z : x + y + z),
     ('single item', 'single_item', ('*', 'text',), DEFAULT_PRECEDENCE, lambda _, x : item(x)),
+    ('items', 'items', ('single_item', 'break'), DEFAULT_PRECEDENCE, lambda x, _ : x),
     # TODO: do this with iterator concatenation to avoid copying and pasting a thousand times the same list
     # Citations
     ('thin bar', 'thin_bar', (THIN_BAR,), DEFAULT_PRECEDENCE, IDENTITY),
